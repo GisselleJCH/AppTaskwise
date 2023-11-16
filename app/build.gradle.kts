2044b49b404949f26b5e0820afa05a56ed107c6d
KenerJoseFonseca <fonsecakenjose@gmail.com>
2023-11-16 00:08:21 -0600

    ActualizacionListas 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -40,7 +43,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.annotation:annotation:1.6.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     testImplementation("junit:junit:4.13.2")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/apptaskwise/Home.kt
+++ app/src/main/java/com/example/apptaskwise/Home.kt
@@ -1,11 +1,86 @@
 package com.example.apptaskwise
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+data class Lista(val nombre: String) // Clase Lista
 
 class Home : AppCompatActivity() {
+
+    private val listas = mutableListOf<Lista>() // Lista de listas
+    private lateinit var contenedorListas: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        contenedorListas = findViewById(R.id.contenedorListas)
+
+        val botonLista = findViewById<TextView>(R.id.botonlista)
+
+        // Manejar clic en "Nueva Lista"
+        botonLista.setOnClickListener {
+            mostrarDialogoNuevaLista()
+        }
     }
+
+    // Función para mostrar un cuadro de diálogo para ingresar el nombre de la nueva lista
+    private fun mostrarDialogoNuevaLista() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Nueva Lista")
+
+        // Configurar el cuadro de diálogo para ingresar el nombre de la lista
+        val input = EditText(this)
+        input.hint = "Ingresa el nombre de la lista"
+        builder.setView(input)
+
+        // Configurar los botones del cuadro de diálogo
+        builder.setPositiveButton("Crear") { _, _ ->
+            val nombreLista = input.text.toString()
+            if (nombreLista.isNotEmpty()) {
+                val nuevaLista = Lista(nombreLista)
+                listas.add(nuevaLista)
+
+                // Log para verificar si la lista se agregó correctamente
+                Log.d("Home", "Lista agregada: ${nuevaLista.nombre}")
+
+                // Actualizar la interfaz de usuario para mostrar las listas
+                mostrarListas()
+            }
+        }
+
+        builder.setNegativeButton("Cancelar") { _, _ ->
+            // No es necesario hacer nada aquí, el cuadro de diálogo se cerrará automáticamente
+        }
+
+        // Mostrar el cuadro de diálogo
+        builder.show()
+    }
+
+    // Función para actualizar la interfaz de usuario y mostrar las listas
+    private fun mostrarListas() {
+        // Limpiar cualquier contenido anterior
+        contenedorListas.removeAllViews()
+
+        // Mostrar las listas en el LinearLayout y el nombre de cada lista
+        for (i in listas.indices) {
+            val lista = listas[i]
+
+            val listaView = TextView(this)
+            listaView.text = "( ${i + 1} )  ${lista.nombre}"
+            listaView.textSize = 18f
+            listaView.setTextColor(ContextCompat.getColor(this, R.color.black))
+            listaView.background = getDrawable(R.drawable.background_rounded_edittext)
+
+            // Agregar el nombre de la lista al LinearLayout
+            contenedorListas.addView(listaView)
+        }
+    }
+
 }
